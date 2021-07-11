9e10c7b253a7ec9abd7c1932e10e281c7c8e7a35
Katarzyna Pędrak <125918@student.uni.opole.pl>
2021-07-11 19:36:50 +0200

    Retrieving data from the database

--- IO/src/io/ConnectionTest.java
+++ IO/src/io/ConnectionTest.java
@@ -1,6 +1,8 @@
 package io;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
@@ -13,9 +15,26 @@ public class ConnectionTest
 	public static void main(String[] args) throws ClassNotFoundException, SQLException
 	{
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		String connectionUrl = "jdbc:sqlserver://DESKTOP-IFL6H2E;database=BazaIO;integratedSecurity=true;";
-		Connection con = DriverManager.getConnection(connectionUrl);
+		String connectionUrl = "jdbc:sqlserver://DESKTOP-IFL6H2E;database=Test1;integratedSecurity=true;";
+//		Connection con = DriverManager.getConnection(connectionUrl);
 		JOptionPane.showMessageDialog(null, "Connected");
+		
+		try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();)
+		{
+			String SQL = "SELECT * FROM tbl_Users";
+			ResultSet rs = stmt.executeQuery(SQL);
+			
+			//Iterate through the data in the result set and display it
+			while(rs.next()) 
+			{
+				System.out.println(rs.getString("UserName") + " "+ rs.getString("Password"));
+			}
+		}
+		//Handle any errors that may have occured
+		catch(SQLException e) 
+		{
+			e.printStackTrace();
+		}
 
 	}
 
